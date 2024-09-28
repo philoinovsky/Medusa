013fd4c01ca4d8180b1babf852671fd4060aaeb8
philoinovsky <1129410550@qq.com>
2024-09-29 06:20:41 +0800

    format

--- medusa/subconverter.py
+++ medusa/subconverter.py
@@ -11,6 +11,45 @@ def b64decode_urlsafe(s: str) -> str:
     return base64.urlsafe_b64decode(s).decode()
 
 
+def handle_ss(ss: ParseResult):
+    return "".join(
+        (
+            "forward=",
+            ss.scheme,
+            "://",
+            ss.username,
+            ":",
+            ss.password,
+            "@",
+            ss.hostname,
+            ":",
+            str(ss.port),
+            "#",
+            unquote(ss.fragment),
+        )
+    )
+
+
+def handle_trojan(tj: ParseResult):
+    return "".join(
+        (
+            "forward=",
+            tj.scheme,
+            "://",
+            tj.username,
+            "@",
+            tj.hostname,
+            ":",
+            str(tj.port),
+            "?",
+            f"serverName={parse_qs(tj.query).get('sni', tj.hostname)}",
+            "&skipVerify=true",
+            "#",
+            unquote(tj.fragment),
+        )
+    )
+
+
 class SubConverter:
     @staticmethod
     def dedup_urls(urls):
@@ -48,43 +87,6 @@ class SubConverter:
 
     @staticmethod
     def _to_glider(parse_results: List[ParseResult]):
-        def handle_ss(ss: ParseResult):
-            return "".join(
-                (
-                    "forward=",
-                    ss.scheme,
-                    "://",
-                    ss.username,
-                    ":",
-                    ss.password,
-                    "@",
-                    ss.hostname,
-                    ":",
-                    str(ss.port),
-                    "#",
-                    unquote(ss.fragment),
-                )
-            )
-
-        def handle_trojan(tj: ParseResult):
-            return "".join(
-                (
-                    "forward=",
-                    tj.scheme,
-                    "://",
-                    tj.username,
-                    "@",
-                    tj.hostname,
-                    ":",
-                    str(tj.port),
-                    "?",
-                    f"serverName={parse_qs(tj.query).get('sni')}",
-                    "&skip-cert-verify=true",
-                    "#",
-                    unquote(tj.fragment),
-                )
-            )
-
         res = list()
         for r in parse_results:
             logging.info(f"Preprocessing {r}")
